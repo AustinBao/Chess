91afe4f01919b04a146589749dadd2cbaaff4ec0
Austin Bao <austin_dun@outlook.com>
2024-09-18 10:39:14 -0600

    first commit

---
+++ ChessEngine.py
@@ -0,0 +1,19 @@
+# Stores all the information about the state of a chess game. Also determines valid moves + move log
+
+class GameState():
+    def __init__(self):
+        # 8x8 two-dimensional list. each element in the list has 2 characters.
+        # the first is the color, the second is the type of piece.
+        # "--" represents empty space
+        self.board = [
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
+        ]
+        self.whiteToMove = True
+        self.moveLog = []
---
+++ ChessMain.py
@@ -0,0 +1,2 @@
+import pygame as p
+WIDTH = HEIGHT = 512  #400 also works
