a765994e73d3ec3d351a772aec85be588ba8687e
Austin Bao <austin_dun@outlook.com>
2024-09-18 11:30:50 -0600

    added better variablity with board color

--- ChessMain.py
+++ ChessMain.py
@@ -3,17 +3,20 @@
 import pygame as p
 import ChessEngine
 
-WIDTH = HEIGHT = 512  #400 also works
+WIDTH = HEIGHT = 512  # 400 also works
 DIMENSION = 8
 SQ_SIZE = HEIGHT // DIMENSION
 MAX_FPS = 15
 IMAGES = {}
 
+
 def loadImages():
-    pieces = ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR", "bP", "wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR", "wP"]
+    pieces = ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR", "bP", "wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR",
+              "wP"]
     for piece in pieces:
         IMAGES[piece] = p.transform.scale(p.image.load("images/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
     #     we can now access an image by keying the dict
+
 
 def main():
     p.init()
@@ -21,7 +24,7 @@ def main():
     clock = p.time.Clock()
     screen.fill(p.Color("white"))
     gs = ChessEngine.GameState()
-    loadImages() #only does this once since its very taxing
+    loadImages()  # only does this once since its very taxing
     running = True
     while running:
         for e in p.event.get():
@@ -31,29 +34,25 @@ def main():
         clock.tick(MAX_FPS)
         p.display.flip()
 
+
 def drawGameState(screen, gs):
-    drawBoard(screen) # draw squares on the board
+    drawBoard(screen)  # draw squares on the board
     # add in pieces highlighting or move suggestions
-    drawPieces(screen, gs.board) # draw pieces on top of the board
+    drawPieces(screen, gs.board)  # draw pieces on top of the board
+
 
 def drawBoard(screen):
-    GREY = (169,169,169)
-    skip = False
+    colors = [p.Color("white"), p.Color("gray")]
     for c in range(DIMENSION):
-        if skip:
-            skip = False
-        else:
-            skip = True
         for r in range(DIMENSION):
-            if skip:
-                skip = False
-                continue
-            square = p.Rect(r * WIDTH//DIMENSION, c * HEIGHT//DIMENSION, SQ_SIZE, SQ_SIZE)
-            p.draw.rect(screen, GREY, square)
-            skip = True
+            color = colors[(r + c) % 2]
+            square = p.Rect(r * SQ_SIZE, c * SQ_SIZE, SQ_SIZE, SQ_SIZE)
+            p.draw.rect(screen, color, square)
+
 
 def drawPieces(screen, board):
     pass
+
 
 if __name__ == "__main__":
     main()
